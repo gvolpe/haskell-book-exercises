4b3f0363c10c22ac84dbee30f922202bffa17e13
Gabriel Volpe <volpegabriel@gmail.com>
2018-03-31 19:29:56 +0900

    Chapter 21: Traversable, done.

--- chapter21/exercises.hs
+++ chapter21/exercises.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE UndecidableInstances #-} -- This is needed for all the constrains (Functor, Foldable). The compiler will tell you need FlexibleContexts instead.
 
 import Data.Monoid
 
@@ -24,22 +24,35 @@ instance (Functor List, Foldable List) => Traversable List where
   traverse _ Nil         = pure $ Nil
   traverse f (Cons x xs) = Cons <$> f x <*> traverse f xs
 
---data Three a b c = Three a b c
+data Three a b c = Three a b c deriving (Eq, Ord, Show)
 
---data Three' a b = Three' a b b
+instance (Functor (Three a b), Foldable (Three a b)) => Traversable (Three a b) where
+  traverse f (Three a b c) = Three a b <$> f c
 
---data S n a = S (n a) a
+data Three' a b = Three' a b b deriving (Eq, Ord, Show)
 
---instance Traversable n => Traversable (S n) where
---traverse = undefined
+instance (Functor (Three' a), Foldable (Three' a)) => Traversable (Three' a) where
+  traverse f (Three' a b c) = Three' a <$> f b <*> f c
 
---data Tree a = Empty | Leaf a | Node (Tree a) a (Tree a) deriving (Eq, Show)
+data S n a = S (n a) a deriving (Eq, Ord, Show)
 
---instance Functor Tree where
---fmap = undefined
+instance (Functor (S n), Foldable (S n), Traversable n) => Traversable (S n) where
+  traverse f (S x y) = S <$> traverse f x <*> f y
 
---instance Foldable Tree where
---foldMap = undefined
+data Tree a = Empty | Leaf a | Node (Tree a) a (Tree a) deriving (Eq, Ord, Show)
 
---instance Traversable Tree where
---traverse = undefined
+instance Functor Tree where
+  fmap _ Empty        = Empty
+  fmap f (Leaf x)     = Leaf (f x)
+  fmap f (Node l x r) = Node (fmap f l) (f x) (fmap f r)
+
+instance Foldable Tree where
+  foldMap _ Empty        = mempty
+  foldMap f (Leaf x)     = f x
+  foldMap f (Node l x r) = (foldMap f l) <> (f x) <> (foldMap f r)
+
+instance Traversable Tree where
+  traverse _ Empty        = pure $ Empty
+  traverse f (Leaf x)     = Leaf <$> f x
+  traverse f (Node l x r) = Node <$> traverse f l <*> f x <*> traverse f r
+
